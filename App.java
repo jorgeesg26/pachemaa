89041a494192ca35b10b5e0df4424dc6daf36280
jorgeesg26 <145496908+jorgeesg26@users.noreply.github.com>
2024-04-24 19:26:42 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,47 +1,56 @@
-public class App {
-public static void main(String[] args) {
-int n;
-boolean capicua = false;
-System.out.print("Por favor, introduzca un número entero (de 5 cifras como máximo): ");
-n = Integer.parseInt(System.console().readLine());
-// número de una cifra
-if (n < 10) {
-capicua = true;
-}
-// número de dos cifras
-if ((n >= 10) && (n < 100)) {
-if ((n / 10) == (n % 10)) {
-capicua = true;
-}
-}
-// número de tres cifras
-if ((n >= 100) && (n < 1000)) {
-if ((n / 100) == (n % 10)) {
-capicua = true;
-}
-}
-// número de cuatro cifras
-if ((n >= 1000) && (n < 10000)) {
-if (((n / 1000) == (n % 10)) && ((( n / 100 ) % 10)== (( n / 10) % 10))) {
-capicua = true;
-}
-}
-// número de cinco cifras
-if (n >= 10000) {
-if (((n / 10000) == (n % 10) ) && ((((n / 1000) % 10)) == ((n / 10) % 10))) {
-capicua = true;
-}
-}
-if (capicua) {
-System.out.println("El número introducido es capicúa.");
-} else {
-System.out.println("El número introducido no es capicúa.");
-}
-}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemon;
+
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.Reader;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+/**
+ *
+ * @author Jorge
+ */
+public class Gestorpokemon {
+
+    private final ArrayList<Pokemon> arrayPokemon;
+
+    public Gestorpokemon() {
+        this.arrayPokemon = new ArrayList<Pokemon>();
+
+    }
+
+    public ArrayList<Pokemon> devuelvearray() {
+        return arrayPokemon;
+    }
+    public void importarJSON(String nombreFichero) {
+    try {
+      JSONParser parser = new JSONParser();
+      Reader reader = new FileReader(nombreFichero);
+      Object jsonObj = parser.parse(reader);
+      JSONObject jsonObject = (JSONObject) jsonObj;
+      
+      JSONArray manolito =(JSONArray) jsonObject.get("pokemon");
+      manolito.forEach(entry ->{ 
+      JSONObject objetopokemon = (JSONObject) entry;
+      String nombre =(String) objetopokemon.get("name");
+      String imagen =(String) objetopokemon.get("img");
+      String peso =(String) objetopokemon.get("weight");
+      Long id =(Long) objetopokemon.get("id");
+      Pokemon noe = new Pokemon(nombre, imagen, peso, id);
+      arrayPokemon.add(noe);
+      });
+      
+      
+
+      // Cerramos el lector
+      reader.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
 }
 
-        }
-      }
-    }
-}
-}
