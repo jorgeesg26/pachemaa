3e3eacc35dbfd6f84824f94c55f417cf2e5ef906
jorgeesg26 <145496908+jorgeesg26@users.noreply.github.com>
2024-04-24 19:34:20 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,56 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package pokemon;
+import java.util.Scanner;
 
-import java.util.ArrayList;
-import java.io.FileReader;
-import java.io.Reader;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-/**
- *
- * @author Jorge
- */
-public class Gestorpokemon {
+public class App {
 
-    private final ArrayList<Pokemon> arrayPokemon;
-
-    public Gestorpokemon() {
-        this.arrayPokemon = new ArrayList<Pokemon>();
-
-    }
-
-    public ArrayList<Pokemon> devuelvearray() {
-        return arrayPokemon;
-    }
-    public void importarJSON(String nombreFichero) {
-    try {
-      JSONParser parser = new JSONParser();
-      Reader reader = new FileReader(nombreFichero);
-      Object jsonObj = parser.parse(reader);
-      JSONObject jsonObject = (JSONObject) jsonObj;
-      
-      JSONArray manolito =(JSONArray) jsonObject.get("pokemon");
-      manolito.forEach(entry ->{ 
-      JSONObject objetopokemon = (JSONObject) entry;
-      String nombre =(String) objetopokemon.get("name");
-      String imagen =(String) objetopokemon.get("img");
-      String peso =(String) objetopokemon.get("weight");
-      Long id =(Long) objetopokemon.get("id");
-      Pokemon noe = new Pokemon(nombre, imagen, peso, id);
-      arrayPokemon.add(noe);
-      });
-      
-      
-
-      // Cerramos el lector
-      reader.close();
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Introduce un número: ");
+    int numero = sc.nextInt();
+    System.out.print("Simple o en color(s/c) ");
+    String simpleocolor = sc.next();
+    sc.close();
+    if (simpleocolor.equals("s")) {
+      if (numero < 0) {
+        System.out.println("Por favor,introduzca un número positivo");
+      }
+      int invertido = 0;
+      int maxDigito = 0;
+      while (numero > 0) {
+        // multiplicamos el invertido por 10 y le sumamos el resto de la división por 10
+        invertido = invertido * 10 + numero % 10;
+        numero = numero / 10;
+        if (numero % 10 > maxDigito) {
+          //  actualizamos el maxdigito y actualizamos el número dividiéndolo por 10
+          maxDigito = numero % 10;
+        }
+        while (invertido > 0) {
+          System.out.println(" ---");
+          System.out.println("| " + invertido % 10 + " |");
+          System.out.println(" ---");
+          // actualizamos el invertido dividiéndolo por 10
+          invertido = invertido / 10;
+        }
+      }
+    } else if (simpleocolor.equals("c")) {}
   }
 }
-
